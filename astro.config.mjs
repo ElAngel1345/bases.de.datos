4e66ad19124d7eda8db3c74e07c4ad1639332deb
ElAngel1345 <afalvarezb@unbosque.edu.co>
2026-02-15 11:13:29 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,11 @@ import edgeoneAdapter from '@edgeone/astro';
 
 // https://astro.build/config
 export default defineConfig({
+import { defineConfig } from 'astro/config';
+export default defineConfig({
+  site: 'https://bases.de.datos',
+  base: 'my-repo',
+});
   adapter: edgeoneAdapter(),
   output: 'static',
   integrations: [react()]
